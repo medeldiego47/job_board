e3eced27623a691b2f3872370a09e9a52c6739b7
DiegoM <medeldiego47@yahoo.com>
2022-11-03 20:52:51 -0500

    initialized files

--- Develop/Develop/server.js
+++ Develop/Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+require('dotenv').config();
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.DB_Secret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
