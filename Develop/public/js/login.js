72f10f3f723f8df0b1608f74aa7e3120f545ace8
DiegoM <medeldiego47@yahoo.com>
2022-11-09 19:02:44 -0600

    created login and logout link now working

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -4,8 +4,8 @@ async function loginFormHandler(event) {
     const name = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (username && password) {
-      const response = await fetch('/api/user/login', {
+    if (name && password) {
+      const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
           name,
